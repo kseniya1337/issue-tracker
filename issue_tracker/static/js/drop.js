116dfeaa3233ed6c716b4a1d6d48c6043876ce88
Kseniya Kochneva <ks3nya1337@gmail.com>
2019-02-20 23:41:19 +0300

    version with comments

--- issue_tracker/static/js/drop.js
+++ issue_tracker/static/js/drop.js
@@ -1,20 +1,10 @@
-// $('.dashboard .col-3').sortable({
-//     stop: function (e, ui) {
-//         console.log('stop', e, ui)
-//
-//         console.log($(ui.item).data('id'));
-//
-//         $.ajax({
-//             url: '/api/change_position',
-//             data: {
-//                 issue_id: $(ui.item).data('id'),
-//                 status: $(ui.item).closest('.column.col-3').data('status')
-//             },
-//             method: 'post',
-//             success: function (response) {
-//                 console.log(response);
-//             }
-//         })
-//     },
-//     connectWith: '.dashboard .col-3'
-// });
+$(function() {
+    $('.drop_element').draggable({
+    });
+    $('#block2').droppable({
+        hoverClass: 'dropHere'
+        ,drop: function(event, ui) {
+            $(this).append($('<div class="textBlock">' + ui.draggable.html() + '</div>'));
+        }
+    });
+});
